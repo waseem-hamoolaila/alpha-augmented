13bb262dceccbceabf00c4d9624ab9494273e604
waseem hamoolaila <waseem.hamoolaila@kuwaitnet.dev>
2023-08-19 15:46:49 +0400

    new shapes

--- processor/packages.py
+++ processor/packages.py
@@ -7,7 +7,15 @@ from .fitting_utils import Package
 packages = [
     Package([[1, 1], [1, 1]], color="#9928b8", identifier=4),
     Package([[1, 1, 1], [1, 0, 0]], color="#0ee607", identifier=1),
-    Package([[1, 1, 1, 0], [0, 1, 1, 1]], color="#d11717", identifier=2),
-    Package([[0, 1, 1, 0], [0, 1, 0, 1]], color="#284cb8", identifier=3),
+    Package([[1, 1, 1, 1]], color="#d11717", identifier=2),
     Package([[1], [1], [1], [1]], color="#289eb8", identifier=5),
+    Package([[0, 1, 0], [1, 1, 1]], color="#284cb8", identifier=3),
+    Package([[1, 1, 1], [0, 0, 1]], color="#284cb8", identifier=6),
+    Package([[1, 1], [0, 1], [0, 1]], color="#284cb8", identifier=7),
+    Package([[0, 1], [0, 1], [1, 1]], color="#284cb8", identifier=8),
+    Package([[1, 1, 0], [0, 1, 1]], color="#284cb8", identifier=9),
+    Package([[1, 0], [1, 1], [0, 1]], color="#284cb8", identifier=7),
+    Package([[1, 1], [0, 1], [0, 1]], color="#284cb8", identifier=7),
+    Package([[1, 1, 1], [0, 0, 1]], color="#284cb8", identifier=7),
+    Package([[1, 1, 1], [0, 1, 0]], color="#284cb8", identifier=7),
 ]
